1c23f482bdde0fff6393981b2c19e51af26c0be1
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-04-09 09:28:29 -0300

    Update AskHere

---
+++ script.js
@@ -0,0 +1,10 @@
+1 function find_max(nums) {
+2   let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
+3   for (let num of nums) {
+4       if (num > max_num) {
+5           num = max_num
+6       }
+7   }
+8      return max_num;
+    9
+};
